4806de3060b75febfe50889604b5d40dec6e3a62
gijsschot <gijsschot@gmail.com>
2023-10-19 05:49:38 +0200

    added double quotes

--- tests/test_find_bugs.py
+++ tests/test_find_bugs.py
@@ -14,7 +14,7 @@ def magic_function(a, b):
 
 class TestMagicFunction(unittest.TestCase):
     def test_function(self):
-        '''Test for magic_function. It tests 5 cases.'''
+        """Test for magic_function. It tests 5 cases."""
         for a, b, answer in ((1, 1, 1), (2, 1, 2), (1, 3, 0.3333333), (-2, 1, -2), (2, 0, np.inf)):
             self.assertEqual(magic_function(a, b), answer,
                              msg=f"division of {a} by {b} does not equal {answer}.")
